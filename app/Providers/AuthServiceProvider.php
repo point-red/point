88d36d480ea2a2bc954fcb8399d76556026d51e1
Martien Dermawan Tanama <martiendt@gmail.com>
2017-10-30 01:35:42 +0700

    update auth routes prefix

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
 use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
@@ -26,6 +27,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        Passport::routes();
+        Route::group(['prefix' => 'api/v1'], function() {
+            Passport::routes();
+        });
     }
 }

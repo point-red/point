f952b1460dc4dc2f39755af1348287537d11543a
Martien Dermawan Tanama <martiendt@gmail.com>
2017-11-02 17:41:32 +0700

    fix format example test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,16 +3,11 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
+    /** @test */
+    public function example()
     {
         $response = $this->get('/');
 
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -3,16 +3,11 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
+    /** @test */
+    public function example()
     {
         $this->assertTrue(true);
     }

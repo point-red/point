538b1e57891923b824069b6690a035daf6e019cf
Martien Dermawan Tanama <martiendt@gmail.com>
2017-11-25 17:34:27 +0700

    add cors domain environment

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,7 +16,7 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', 'http://localhost:8080')
+            ->header('Access-Control-Allow-Origin', env('CORS_DOMAIN'))
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
             ->header('Access-Control-Allow-Credentials', 'true');

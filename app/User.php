cb10c963e895826993ecd8ff40bf2ccf999cdf63
Martien Dermawan Tanama <martiendt@gmail.com>
2018-06-21 05:56:49 +0700

    Apply fixes from StyleCI (#30)
    [ci skip] [skip ci]

--- app/User.php
+++ app/User.php
@@ -3,9 +3,9 @@
 namespace App;
 
 use Laravel\Passport\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {

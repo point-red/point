e57bb8d4e87c3828acb4be7a1cfeaeab6d469920
Martien Dermawan Tanama <martiendt@gmail.com>
2018-06-30 22:48:20 +0700

    allow PATCH request

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,7 @@ class Cors
     {
         return $next($request)
             ->header('Access-Control-Allow-Origin', env('CORS_DOMAIN'))
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Tenant, Authorization, Content-Type')
             ->header('Access-Control-Allow-Credentials', 'true');
     }

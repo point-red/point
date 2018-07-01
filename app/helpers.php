de7c3eff28152484d2ba7360aacb6f3bdbadb416
Martien Dermawan Tanama <martiendt@gmail.com>
2018-07-01 20:46:02 +0700

    add helpers

---
+++ app/helpers.php
@@ -0,0 +1,16 @@
+<?php
+
+if (! function_exists('log_array')) {
+    /**
+     * Get database size
+     *
+     * @param        $databaseName
+     * @param string $connection
+     *
+     * @return float
+     */
+    function log_object($object)
+    {
+        info(print_r($object, true));
+    }
+}

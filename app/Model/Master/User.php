a7fb8dc24c5bdc22195752ca81fc136c846b18eb
Martien Dermawan Tanama <martiendt@gmail.com>
2018-07-11 09:06:23 +0700

    using guard api

--- app/Model/Master/User.php
+++ app/Model/Master/User.php
@@ -3,14 +3,13 @@
 namespace App\Model\Master;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Collection;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Model
 {
     protected $connection = 'tenant';
 
-    protected $guard_name = 'web';
+    protected $guard_name = 'api';
 
     use HasRoles;
 

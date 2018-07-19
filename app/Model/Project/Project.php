2261e888826426a90e6da72a9f3f556aa32f00a6
Martien Dermawan Tanama <martiendt@gmail.com>
2018-07-19 18:23:51 +0700

    add eloquent relationship

--- app/Model/Project/Project.php
+++ app/Model/Project/Project.php
@@ -2,9 +2,18 @@
 
 namespace App\Model\Project;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
     protected $connection = 'mysql';
+
+    /**
+     * Get the owner that owns the project.
+     */
+    public function owner()
+    {
+        return $this->belongsTo(get_class(new User()), 'owner_id');
+    }
 }

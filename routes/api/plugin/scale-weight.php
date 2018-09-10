11cb37848f38bc4ac73727877fb8ecd40fef35a5
Martien Dermawan Tanama <martiendt@gmail.com>
2018-09-10 11:11:54 +0700

    fix capitalize

--- routes/api/plugin/scale-weight.php
+++ routes/api/plugin/scale-weight.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::prefix('plugin')->namespace('plugin')->group(function () {
+Route::prefix('plugin')->namespace('Plugin')->group(function () {
     Route::prefix('scale-weight')->namespace('ScaleWeight')->group(function () {
         Route::apiResource('trucks', 'ScaleWeightTruckController');
         Route::apiResource('items', 'ScaleWeightItemController');

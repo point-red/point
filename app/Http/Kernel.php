8bcb99772751f10bf43bd19fcb3480f5417635f7
Martien Dermawan Tanama <martiendt@gmail.com>
2018-09-13 07:10:32 +0700

    add http log request

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,6 +21,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\Cors::class,
         \App\Http\Middleware\TenantMiddleware::class,
+        \App\Http\Middleware\LogRequest::class,
     ];
 
     /**
---
+++ app/Http/Middleware/LogRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class LogRequest
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request);
+    }
+
+    public function terminate($request, $response)
+    {
+        $start = LARAVEL_START;
+        $end = microtime(true);
+        $diff = $end - $start;
+
+        log_object('[' . $request->method() . '] ' . $request->url() . ' ' . $diff);
+    }
+}

e51424af8fea455c656e6ccaa66a0bcf3d0170a5
Martien Dermawan Tanama <martiendt@gmail.com>
2018-10-15 16:15:59 +0700

    update relationship

--- app/Http/Controllers/Api/Master/CustomerGroupController.php
+++ app/Http/Controllers/Api/Master/CustomerGroupController.php
@@ -18,16 +18,6 @@ class CustomerGroupController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request

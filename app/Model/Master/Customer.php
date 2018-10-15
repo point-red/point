36059ebaf51865ac4f67bf413c3e806ed4bb7318
Martien Dermawan Tanama <martiendt@gmail.com>
2018-10-15 16:15:59 +0700

    update relationship

--- app/Model/Master/Customer.php
+++ app/Model/Master/Customer.php
@@ -9,4 +9,12 @@ class Customer extends Model
     protected $connection = 'tenant';
 
     protected $fillable = ['name', 'tax_identification_number'];
+
+    /**
+     * Get the group that owns the customer.
+     */
+    public function group()
+    {
+        return $this->belongsTo(get_class(new CustomerGroup()), 'customer_group_id');
+    }
 }

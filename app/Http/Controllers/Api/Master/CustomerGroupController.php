2c4ccddca5bdcf0babf17bbb3238ebb584a88e0c
Martien Dermawan Tanama <martiendt@gmail.com>
2018-10-16 10:49:44 +0700

    add customer group endpoint

--- app/Http/Controllers/Api/Master/CustomerGroupController.php
+++ app/Http/Controllers/Api/Master/CustomerGroupController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers\Api\Master;
 
+use App\Http\Requests\Master\CustomerGroup\StoreCustomerGroupRequest;
+use App\Http\Resources\ApiCollection;
+use App\Http\Resources\ApiResource;
+use App\Model\Master\CustomerGroup;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -10,44 +14,41 @@ class CustomerGroupController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return ApiCollection
      */
     public function index()
     {
-        //
+        $customerGroups = CustomerGroup::all();
+
+        return new ApiCollection($customerGroups);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param StoreCustomerGroupRequest $request
+     * @return ApiResource
      */
-    public function store(Request $request)
+    public function store(StoreCustomerGroupRequest $request)
     {
-        //
+        $customerGroup = new CustomerGroup;
+        $customerGroup->fill($request->all());
+        $customerGroup->save();
+
+        return new ApiResource($customerGroup);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return ApiResource
      */
     public function show($id)
     {
-        //
-    }
+        $customerGroup = CustomerGroup::findOrFail($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        return new ApiResource($customerGroup);
     }
 
     /**
@@ -55,11 +56,15 @@ class CustomerGroupController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return ApiResource
      */
     public function update(Request $request, $id)
     {
-        //
+        $customerGroup = CustomerGroup::findOrFail($id);
+        $customerGroup->fill($request->all());
+        $customerGroup->save();
+
+        return new ApiResource($customerGroup);
     }
 
     /**
@@ -70,6 +75,9 @@ class CustomerGroupController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $customerGroup = CustomerGroup::findOrFail($id);
+        $customerGroup->delete();
+
+        return response()->json([], 204);
     }
 }
--- app/Http/Requests/Master/CustomerGroup/StoreCustomerGroupRequest.php
+++ app/Http/Requests/Master/CustomerGroup/StoreCustomerGroupRequest.php
@@ -13,7 +13,7 @@ class StoreCustomerGroupRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,8 @@ class StoreCustomerGroupRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'code' => 'required',
+            'name' => 'required'
         ];
     }
 }

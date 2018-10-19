de6432132a05f7935cad3636f5786f18dbdd1978
Martien Dermawan Tanama <martiendt@gmail.com>
2018-10-19 14:54:04 +0700

    fix seeder

--- database/seeds/EmployeeSeeder.php
+++ database/seeds/EmployeeSeeder.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Seeder;
 use App\Model\HumanResource\Employee\EmployeeGender;
 use App\Model\HumanResource\Employee\EmployeeReligion;
 use App\Model\HumanResource\Employee\EmployeeMaritalStatus;
+use Illuminate\Support\Facades\DB;
 
 class EmployeeSeeder extends Seeder
 {
@@ -14,25 +15,33 @@ class EmployeeSeeder extends Seeder
      */
     public function run()
     {
+        $owner = \App\Model\Master\User::first();
+
         $religions = ['Christian', 'Catholic', 'Islam', 'Buddha', 'Hindu'];
         for ($i = 0; $i < count($religions); $i++) {
-            $employeeReligion = new EmployeeReligion;
-            $employeeReligion->name = $religions[$i];
-            $employeeReligion->save();
+            DB::table('employee_religions')->insert([
+                'name' => $religions[$i],
+                'created_by' => $owner->id,
+                'updated_by' => $owner->id,
+            ]);
         }
 
         $maritalStatues = ['Single', 'Married'];
         for ($i = 0; $i < count($maritalStatues); $i++) {
-            $employeeMaritalStatus = new EmployeeMaritalStatus;
-            $employeeMaritalStatus->name = $maritalStatues[$i];
-            $employeeMaritalStatus->save();
+            DB::table('employee_marital_statuses')->insert([
+                'name' => $maritalStatues[$i],
+                'created_by' => $owner->id,
+                'updated_by' => $owner->id,
+            ]);
         }
 
         $genders = ['Male', 'Female'];
         for ($i = 0; $i < count($genders); $i++) {
-            $employeeGender = new EmployeeGender;
-            $employeeGender->name = $genders[$i];
-            $employeeGender->save();
+            DB::table('employee_genders')->insert([
+                'name' => $genders[$i],
+                'created_by' => $owner->id,
+                'updated_by' => $owner->id,
+            ]);
         }
     }
 }

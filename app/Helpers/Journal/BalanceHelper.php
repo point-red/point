44e79d0a318ec5f562c5305d8804c0a4038b422f
Martien Dermawan Tanama <martiendt@gmail.com>
2018-10-26 15:04:46 +0700

    add opening balance query

--- app/Helpers/Journal/BalanceHelper.php
+++ app/Helpers/Journal/BalanceHelper.php
@@ -9,7 +9,20 @@ class BalanceHelper
 {
     public static function openingBalance($date, $options = [])
     {
-        //
+        $journals = Journal::select('chart_of_account_id')
+            ->addSelect(DB::raw('max(date) as date'))
+            ->addSelect(DB::raw('sum(debit) as debit'))
+            ->addSelect(DB::raw('sum(credit) as credit'))
+            ->with('chartOfAccount')
+            ->where('date', '<', date('Y-m-d 00:00:00', strtotime($date)))
+            ->groupBy('chart_of_account_id');
+
+        // Exclude account that doesn't have any value
+        if (in_array('without_zero', $options)) {
+            $journals = $journals->hasValue();
+        }
+
+        return $journals->get();
     }
 
     public static function endingBalance($date, $options = [])

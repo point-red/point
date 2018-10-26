5d038c30fab79c8bab4a7442d70957a1f7430ee1
Martien Dermawan Tanama <martiendt@gmail.com>
2018-10-26 16:19:37 +0700

    add range balance method for mutation data

--- app/Helpers/Journal/BalanceHelper.php
+++ app/Helpers/Journal/BalanceHelper.php
@@ -25,6 +25,25 @@ class BalanceHelper
         return $journals->get();
     }
 
+    public static function rangeBalance($fromDate, $untilDate, $options = [])
+    {
+        $journals = Journal::select('chart_of_account_id')
+            ->addSelect(DB::raw('max(date) as date'))
+            ->addSelect(DB::raw('sum(debit) as debit'))
+            ->addSelect(DB::raw('sum(credit) as credit'))
+            ->with('chartOfAccount')
+            ->where('date', '>=', date('Y-m-d 00:00:00', strtotime($fromDate)))
+            ->where('date', '<', date('Y-m-d 00:00:00', strtotime($untilDate)))
+            ->groupBy('chart_of_account_id');
+
+        // Exclude account that doesn't have any value
+        if (in_array('without_zero', $options)) {
+            $journals = $journals->hasValue();
+        }
+
+        return $journals->get();
+    }
+
     public static function endingBalance($date, $options = [])
     {
         $journals = Journal::select('chart_of_account_id')

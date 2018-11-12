5e33b432106b0300e0d0a9c93dba34ed6a9fc1f5
Martien Dermawan Tanama <martiendt@gmail.com>
2018-11-12 17:01:54 +0700

    add empty report controller

---
+++ app/Http/Controllers/Api/Plugin/PinPoint/Report/SalesReportController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\Plugin\PinPoint\Report;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SalesReportController extends Controller
+{
+    //
+}

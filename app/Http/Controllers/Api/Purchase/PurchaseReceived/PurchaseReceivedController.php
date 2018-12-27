0fa2391b201cfbeaebb7c828cfd45ca64f365e33
Martien Dermawan Tanama <martiendt@gmail.com>
2018-12-27 18:51:57 +0700

    fix import

--- app/Http/Controllers/Api/Purchase/PurchaseReceived/PurchaseReceivedController.php
+++ app/Http/Controllers/Api/Purchase/PurchaseReceived/PurchaseReceivedController.php
@@ -7,6 +7,7 @@ use App\Http\Resources\ApiResource;
 use App\Model\Purchase\PurchaseReceived\PurchaseReceived;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class PurchaseReceivedController extends Controller
 {

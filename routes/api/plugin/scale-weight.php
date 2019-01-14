5a69e33ea43efb761908b9cf1587be583f1a4c6a
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2019-01-14 10:15:48 +0700

    update api route

--- routes/api/plugin/scale-weight.php
+++ routes/api/plugin/scale-weight.php
@@ -6,5 +6,7 @@ Route::prefix('plugin')->namespace('Plugin')->group(function () {
         Route::apiResource('trucks', 'ScaleWeightTruckController');
         Route::post('items/export', 'ScaleWeightItemExportController@export');
         Route::apiResource('items', 'ScaleWeightItemController');
+        Route::post('merge/export', 'ScaleWeightMergeExportController@export');
+        Route::apiResource('merge', 'ScaleWeightMergeController');
     });
 });

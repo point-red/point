7998b7f53d86f7932e7cdb3023d11bdced66f27f
Christhofer Natalius <christhofer@gradin.co.id>
2019-01-17 14:47:38 +0700

    fixed wrong table names, delivery_notes and delivery_orders without sales_ prefix

--- database/migrations/tenant/2018_12_13_095459_create_sales_invoice_items_table.php
+++ database/migrations/tenant/2018_12_13_095459_create_sales_invoice_items_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateSalesInvoiceItemsTable extends Migration
 {
@@ -16,8 +16,8 @@ class CreateSalesInvoiceItemsTable extends Migration
         Schema::create('sales_invoice_items', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('sales_invoice_id');
-            $table->unsignedInteger('sales_delivery_note_id');
-            $table->unsignedInteger('sales_delivery_note_item_id');
+            $table->unsignedInteger('delivery_note_id');
+            $table->unsignedInteger('delivery_note_item_id');
             $table->unsignedInteger('item_id');
             $table->decimal('quantity', 65, 30);
             $table->decimal('price', 65, 30);
@@ -30,8 +30,8 @@ class CreateSalesInvoiceItemsTable extends Migration
             $table->unsignedInteger('allocation_id')->nullable();
 
             $table->foreign('sales_invoice_id')->references('id')->on('sales_invoices')->onDelete('cascade');
-            $table->foreign('sales_delivery_note_id')->references('id')->on('sales_delivery_notes')->onDelete('cascade');
-            $table->foreign('sales_delivery_note_item_id')->references('id')->on('sales_delivery_note_items')->onDelete('cascade');
+            $table->foreign('delivery_note_id')->references('id')->on('delivery_notes')->onDelete('cascade');
+            $table->foreign('delivery_note_item_id')->references('id')->on('delivery_note_items')->onDelete('cascade');
             $table->foreign('item_id')->references('id')->on('items')->onDelete('restrict');
             $table->foreign('allocation_id')->references('id')->on('allocations')->onDelete('restrict');
         });

4a2ff49c2b8ea649320e7c294565e8a0c4f70a76
Christhofer Natalius <christhofer@gradin.co.id>
2019-01-19 10:36:54 +0700

    fixed error DeliveryOrder class is not imported

--- app/Model/Sales/SalesOrder/SalesOrder.php
+++ app/Model/Sales/SalesOrder/SalesOrder.php
@@ -7,6 +7,8 @@ use App\Model\Master\Customer;
 use App\Model\Master\Warehouse;
 use App\Model\Sales\SalesQuotation\SalesQuotation;
 use App\Model\TransactionModel;
+use App\Model\Sales\DeliveryOrder\DeliveryOrder;
+use App\Model\Sales\DeliveryOrder\DeliveryOrderItem;
 
 class SalesOrder extends TransactionModel
 {

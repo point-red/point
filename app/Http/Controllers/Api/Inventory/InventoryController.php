ba8a3e34540698b4be7bb245a38ec733124b8159
Martien Dermawan Tanama <martiendt@gmail.com>
2019-02-18 15:13:59 +0700

    add mutation stock

--- app/Http/Controllers/Api/Inventory/InventoryController.php
+++ app/Http/Controllers/Api/Inventory/InventoryController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api\Inventory;
 
 use App\Http\Resources\ApiCollection;
-use App\Http\Resources\ApiResource;
 use App\Model\Form;
 use App\Model\Inventory\Inventory;
 use App\Http\Controllers\Controller;
@@ -20,27 +19,11 @@ class InventoryController extends Controller
     public function index(Request $request)
     {
         $inventories = Inventory::join(Form::getTableName(), Form::getTableName('id'), '=', Inventory::getTableName('form_id'))
-            ->groupBy('item_id')
-            ->orderBy('forms.date', 'desc');
+            ->where('item_id', $request->get('item_id'))
+            ->whereBetween('forms.date', [$request->get('date_from'), $request->get('date_to')]);
 
-        // Ignore item that doesn't have any stock
-        if ($request->get('ignore_empty') == true) {
-            $inventories = $inventories->where('total_quantity', '>', 0);
-        }
-
-        $inventories = $inventories->get();
+        $inventories = $inventories->paginate(100);
 
         return new ApiCollection($inventories);
-    }
-
-    public function show(Request $request, $itemId)
-    {
-        $inventory = Inventory::join(Form::getTableName(), Form::getTableName('id'), '=', Inventory::getTableName('form_id'))
-            ->where('item_id', $itemId)
-            ->groupBy('item_id')
-            ->orderBy('forms.date', 'desc')
-            ->first();
-
-        return new ApiResource($inventory);
     }
 }
--- routes/api/inventory.php
+++ routes/api/inventory.php
@@ -2,7 +2,6 @@
 
 Route::prefix('inventory')->namespace('Inventory')->group(function () {
     Route::get('/', 'InventoryController@index');
-    Route::get('/{item_id}', 'InventoryController@show');
     Route::apiResource('inventory-audits', 'InventoryAudit\\AuditController');
     // Route::apiResource('inventory-usages', 'InventoryUsageController');
     // Route::apiResource('inventory-corrections', 'InventoryCorrectionController');

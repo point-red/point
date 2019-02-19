3e1ca7d1ff2c8935a1db59c6556616eed3d12d57
Christhofer Natalius <christhofer@gradin.co.id>
2019-02-19 14:44:37 +0700

    implemented accessor and mutator for date to UTC

--- app/Model/PointModel.php
+++ app/Model/PointModel.php
@@ -4,10 +4,31 @@ namespace App\Model;
 
 use App\Traits\EloquentFilters;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class PointModel extends Model
 {
     use EloquentFilters;
+
+    public function setDateAttribute($value)
+    {
+        $this->attributes['date'] = Carbon::parse($value, request()->header('Timezone'))->timezone('UTC')->toDateTimeString();
+    }
+
+    public function getDateAttribute($value)
+    {
+        return Carbon::parse($value, 'UTC')->timezone(request()->header('Timezone'))->toDateTimeString();
+    }
+
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value, 'UTC')->timezone(request()->header('Timezone'))->toDateTimeString();
+    }
+
+    public function getUpdatedAtAttribute($value)
+    {
+        return Carbon::parse($value, 'UTC')->timezone(request()->header('Timezone'))->toDateTimeString();
+    }
 
     public static function getTableName($column = null)
     {

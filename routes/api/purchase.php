f075cdb7baaf1eed31fb2a40a39c78cbcc50c0fc
Martien Dermawan Tanama <martiendt@gmail.com>
2019-02-26 19:15:41 +0700

    add routes

--- routes/api/purchase.php
+++ routes/api/purchase.php
@@ -1,8 +1,9 @@
 <?php
 
 Route::prefix('purchase')->namespace('Purchase')->group(function () {
+    Route::apiResource('purchase-contracts', 'PurchaseContract\\PurchaseContractController');
     Route::apiResource('purchase-requests', 'PurchaseRequest\\PurchaseRequestController');
-    Route::apiResource('purchase-downpayments', 'PurchaseDownpayment\\PurchaseDownpaymentController');
+    Route::apiResource('purchase-down-payments', 'PurchaseDownPayment\\PurchaseDownPaymentController');
     Route::apiResource('purchase-orders', 'PurchaseOrder\\PurchaseOrderController');
     Route::apiResource('purchase-receives', 'PurchaseReceive\\PurchaseReceiveController');
     Route::apiResource('purchase-invoices', 'PurchaseInvoice\\PurchaseInvoiceController');
--- routes/api/sales.php
+++ routes/api/sales.php
@@ -1,9 +1,10 @@
 <?php
 
 Route::prefix('sales')->namespace('Sales')->group(function () {
+    Route::apiResource('sales-contracts', 'SalesContract\\SalesContractController');
     // Route::apiResource('sales-quotation', 'SalesQuotationController');
     Route::apiResource('sales-orders', 'SalesOrder\\SalesOrderController');
-    // Route::apiResource('downpayment', 'DownpaymentController');
+    Route::apiResource('sales-down-payments', 'SalesDownPayment\\SalesDownPaymentController');
     Route::apiResource('delivery-orders', 'DeliveryOrder\\DeliveryOrderController');
     Route::apiResource('delivery-notes', 'DeliveryNote\\DeliveryNoteController');
     Route::apiResource('sales-invoices', 'SalesInvoice\\SalesInvoiceController');

9920a2d9ceff051f644b4358fc6389af5a8fbd1b
Martien Dermawan Tanama <martiendt@gmail.com>
2019-03-04 21:10:16 +0700

    add master allocation

--- app/Http/Controllers/Api/Master/AllocationController.php
+++ app/Http/Controllers/Api/Master/AllocationController.php
@@ -3,52 +3,70 @@
 namespace App\Http\Controllers\Api\Master;
 
 use Illuminate\Http\Request;
+use App\Model\Master\Allocation;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ApiCollection;
+use App\Http\Resources\ApiResource;
+use App\Http\Requests\Master\Allocation\StoreAllocationRequest;
+use App\Http\Requests\Master\Allocation\UpdateAllocationRequest;
 
 class AllocationController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return ApiCollection
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $allocations = Allocation::eloquentFilter($request);
+
+        $allocations = pagination($allocations, $request->get('limit'));
+
+        return new ApiCollection($allocations);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param StoreAllocationRequest $request
+     * @return ApiResource
      */
-    public function store(Request $request)
+    public function store(StoreAllocationRequest $request)
     {
-        //
+        $allocation = new Allocation();
+        $allocation->name = $request->input('name');
+        $allocation->save();
+
+        return new ApiResource($allocation);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return ApiResource
      */
     public function show($id)
     {
-        //
+        return new ApiResource(Allocation::findOrFail($id));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param UpdateAllocationRequest $request
+     * @param  int $id
+     * @return ApiResource
      */
-    public function update(Request $request, $id)
+    public function update(UpdateAllocationRequest $request, $id)
     {
-        //
+        $allocation = Allocation::findOrFail($id);
+        $allocation->name = $request->input('name');
+        $allocation->save();
+
+        return new ApiResource($allocation);
     }
 
     /**
@@ -59,6 +77,8 @@ class AllocationController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Allocation::findOrFail($id)->delete();
+
+        return response(null, 204);
     }
 }

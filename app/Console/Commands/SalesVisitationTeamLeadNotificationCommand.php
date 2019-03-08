8d9441f54647f6174f468f227e80c2d5c47d0966
Martien Dermawan Tanama <martiendt@gmail.com>
2019-03-08 10:54:58 +0700

    remove hardcoded email name setup

--- app/Console/Commands/SalesVisitationTeamLeadNotificationCommand.php
+++ app/Console/Commands/SalesVisitationTeamLeadNotificationCommand.php
@@ -57,8 +57,7 @@ class SalesVisitationTeamLeadNotificationCommand extends Command
             // Update tenant database name in configuration
             config()->set('database.connections.tenant.database', strtolower($databaseName));
             DB::connection('tenant')->reconnect();
-            config()->set('mail.from.name', capitalize($project->name));
-            
+
             $salesVisitationForm = SalesVisitation::join('forms', 'forms.id', '=', 'pin_point_sales_visitations.form_id')
                 ->with('form')
                 ->select('pin_point_sales_visitations.*');

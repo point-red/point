bf43626efaed090d40b5ca100371354689b637a7
Martien Dermawan Tanama <martiendt@gmail.com>
2019-03-11 15:57:45 +0700

    dummy priority

--- app/Http/Controllers/Api/Master/CustomerController.php
+++ app/Http/Controllers/Api/Master/CustomerController.php
@@ -79,7 +79,7 @@ class CustomerController extends Controller
             if (!$group) {
                 $group = new Group;
                 $group->name = $request->get('group')['name'];
-                $group->type = Customer::class;
+                $group->type = 'Customer';
                 $group->save();
             }
 

3e38b1e0bda5c59136bdd86ce585171781ffa9c3
Martien Dermawan Tanama <martiendt@gmail.com>
2019-03-12 12:39:06 +0700

    fix supplier index

--- app/Http/Controllers/Api/Master/SupplierController.php
+++ app/Http/Controllers/Api/Master/SupplierController.php
@@ -27,21 +27,24 @@ class SupplierController extends Controller
      */
     public function index(Request $request)
     {
-        $suppliers = Supplier::eloquentFilter($request)
-            ->with('groups')
-            ->with('addresses')
-            ->with('emails')
-            ->with('banks')
-            ->with('phones')
-            ->with('contactPersons');
+        $suppliers = Supplier::leftjoin(Address::getTableName(), function ($q) {
+            $q->on(Address::getTableName('addressable_id'), '=', Supplier::getTableName('id'))
+                ->where(Address::getTableName('addressable_type'), Supplier::class);
+        })->leftjoin(Phone::getTableName(), function ($q) {
+            $q->on(Phone::getTableName('phoneable_id'), '=', Supplier::getTableName('id'))
+                ->where(Phone::getTableName('phoneable_type'), Supplier::class);
+        })->select(Supplier::getTableName('*'))
+            ->eloquentFilter($request);
 
         if ($request->get('group_id')) {
-            $suppliers = $suppliers->leftJoin('groupables', 'groupables.groupable_id', '=', 'items.id')
-                ->where('groupables.groupable_type', Supplier::class)
-                ->where('groupables.group_id', '=', 1);
+            $suppliers = $suppliers->join('groupables', function ($q) use ($request) {
+                $q->on('groupables.groupable_id', '=', 'suppliers.id')
+                    ->where('groupables.groupable_type', Supplier::class)
+                    ->where('groupables.group_id', '=', $request->get('group_id'));
+            });
         }
 
-        $suppliers = pagination($suppliers, request()->get('limit'));
+        $suppliers = pagination($suppliers, $request->get('limit'));
 
         return new ApiCollection($suppliers);
     }

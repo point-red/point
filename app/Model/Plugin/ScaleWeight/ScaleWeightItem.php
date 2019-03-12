05069ea6e4c05ada9076d90cf4f51527158c2ee6
Martien Dermawan Tanama <martiendt@gmail.com>
2019-03-12 14:41:12 +0700

    update date

--- app/Model/Plugin/ScaleWeight/ScaleWeightItem.php
+++ app/Model/Plugin/ScaleWeight/ScaleWeightItem.php
@@ -2,6 +2,7 @@
 
 namespace App\Model\Plugin\ScaleWeight;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class ScaleWeightItem extends Model
@@ -9,4 +10,14 @@ class ScaleWeightItem extends Model
     protected $connection = 'tenant';
 
     protected $table = 'scale_weight_items';
+
+    public function setTimeAttribute($value)
+    {
+        $this->attributes['time'] = Carbon::parse($value, request()->header('Timezone'))->timezone('UTC')->toDateTimeString();
+    }
+
+    public function getTimeAttribute($value)
+    {
+        return Carbon::parse($value, 'UTC')->timezone(request()->header('Timezone'))->toDateTimeString();
+    }
 }

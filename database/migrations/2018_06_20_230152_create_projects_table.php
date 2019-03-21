613ab5041bf0575f9c814a3111c0186992cd1032
Martien Dermawan Tanama <martiendt@gmail.com>
2019-03-22 06:07:23 +0700

    add group into project

--- database/migrations/2018_06_20_230152_create_projects_table.php
+++ database/migrations/2018_06_20_230152_create_projects_table.php
@@ -17,6 +17,7 @@ class CreateProjectsTable extends Migration
             $table->increments('id');
             $table->string('code')->unique();
             $table->string('name');
+            $table->string('group')->nullable();
             $table->string('timezone')->default('UTC');
             $table->text('address')->nullable();
             $table->string('phone')->nullable();

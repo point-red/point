94a505b07e55cd33a7d70e2aa8d56d31aa97d6a3
Christhofer Natalius <christhofernatalius@gmail.com>
2019-05-05 10:27:20 +0700

    fixed error Cannot use App\Model\Sales\SalesReturn\SalesReturn as SalesReturn because the name is already in use

--- app/Providers/PolymorphicTypeServiceProvider.php
+++ app/Providers/PolymorphicTypeServiceProvider.php
@@ -9,7 +9,7 @@ use App\Model\Purchase\PurchaseContract\PurchaseContract;
 use App\Model\Purchase\PurchaseDownPayment\PurchaseDownPayment;
 use App\Model\Purchase\PurchaseInvoice\PurchaseInvoice;
 use App\Model\Purchase\PurchaseOrder\PurchaseOrder;
-use App\Model\Purchase\PurchaseReturn\SalesReturn;
+use App\Model\Purchase\PurchaseReturn\PurchaseReturn;
 use App\Model\Sales\SalesContract\SalesContract;
 use App\Model\Sales\SalesDownPayment\SalesDownPayment;
 use App\Model\Sales\SalesInvoice\SalesInvoice;
@@ -35,7 +35,7 @@ class PolymorphicTypeServiceProvider extends ServiceProvider
             'PurchaseContract' => PurchaseContract::class,
             'PurchaseDownPayment' => PurchaseDownPayment::class,
             'PurchaseInvoice' => PurchaseInvoice::class,
-            'PurchaseReturn' => SalesReturn::class,
+            'PurchaseReturn' => PurchaseReturn::class,
             'SalesOrder' => SalesOrder::class,
             'SalesContract' => SalesContract::class,
             'SalesDownPayment' => SalesDownPayment::class,

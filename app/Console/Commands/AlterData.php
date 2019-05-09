3395fd6fd41cd0b231b9268993622f98b742affe
Martien Dermawan Tanama <martiendt@gmail.com>
2019-05-09 14:02:23 +0700

    fix item account

--- app/Console/Commands/AlterData.php
+++ app/Console/Commands/AlterData.php
@@ -2,9 +2,10 @@
 
 namespace App\Console\Commands;
 
+use App\Model\Accounting\ChartOfAccount;
 use App\Model\Master\Item;
-use App\Model\Master\ItemUnit;
 use App\Model\Project\Project;
+use ChartOfAccountSeeder;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Artisan;
@@ -44,40 +45,28 @@ class AlterData extends Command
     {
         $projects = Project::all();
         foreach ($projects as $project) {
-//            $this->line('Clone ' . $project->code);
-//            Artisan::call('tenant:database:backup-clone', ['project_code' => strtolower($project->code)]);
+            $this->line('Clone ' . $project->code);
+            Artisan::call('tenant:database:backup-clone', ['project_code' => strtolower($project->code)]);
             $this->line('Alter '.$project->code);
             config()->set('database.connections.tenant.database', env('DB_DATABASE').'_'.strtolower($project->code));
             DB::connection('tenant')->reconnect();
 
-            $items = Item::all();
+            $chartOfAccounts = ChartOfAccount::all();
 
-            foreach ($items as $item) {
-                $this->line($item->id.' '.$item->units);
-                if (count($item->units) == 0) {
-                    $this->line($item->id.' add unit');
-                    $unit = new ItemUnit;
-                    $unit->label = 'pcs';
-                    $unit->name = 'pcs';
-                    $unit->item_id = $item->id;
-                    $unit->converter = 1;
-                    $unit->save();
-                }
+            if ($chartOfAccounts->count() == 0) {
+                $this->call(ChartOfAccountSeeder::class);
             }
 
-            // TODO: ADD TAXABLE COLUMN IN ITEMS AND SERVICES
-            // TODO: ADD NOTES IN FORM
-            // TODO: EDIT EDITED NOTES IN FORM (FROM STRING TO TEXT)
-            // TODO: ITEM (CODE & BARCODE UNIQUE)
-            // $table->boolean('unit_default')->default(false);
-            // $table->boolean('unit_default_purchase')->default(false);
-            // $table->boolean('unit_default_sales')->default(false);
-//            ALTER TABLE pin_point_sales_visitations ADD COLUMN is_repeat_order BOOLEAN DEFAULT false
-//            DB::connection('tenant')->statement('ALTER TABLE `groups` ADD COLUMN code varchar(255)');
-//            DB::connection('tenant')->statement('ALTER TABLE `pin_point_sales_visitations` ADD COLUMN is_repeat_order BOOLEAN DEFAULT false AFTER payment_received');
+            $items = Item::all();
 
-//            $this->line('Migrate ' . $project->code);
-//            Artisan::call('tenant:migrate', ['db_name' => env('DB_DATABASE').'_' . strtolower($project->code)]);
+            $account = ChartOfAccount::where('name', 'sediaan barang jadi (manufaktur)')->first();
+
+            foreach ($items as $item) {
+                if ($item->chart_of_account_id == null) {
+                    $item->chart_of_account_id = $account->id;
+                    $item->save();
+                }
+            }
         }
     }
 }

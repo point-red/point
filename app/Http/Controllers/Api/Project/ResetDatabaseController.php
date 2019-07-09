9225846f130086cb9053d64abefba514a0d6e3ab
Martien Dermawan Tanama <martiendt@gmail.com>
2019-07-09 09:24:06 +0700

    add reset database controller

---
+++ app/Http/Controllers/Api/Project/ResetDatabaseController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api\Project;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Artisan;
+
+class ResetDatabaseController extends Controller
+{
+    public function index(Request $request)
+    {
+        Artisan::call('tenant:database:reset', [
+            'project_code' => 'dev'
+        ]);
+    }
+}
--- routes/api/project.php
+++ routes/api/project.php
@@ -4,6 +4,7 @@ Route::prefix('project')->namespace('Project')->group(function () {
     Route::apiResource('projects', 'ProjectController');
     Route::get('projects/{id}/database/backups', 'DatabaseBackupController@index');
     Route::post('projects/{id}/database/backups', 'DatabaseBackupController@store');
+    Route::post('projects/{id}/database/reset', 'ResetDatabaseController@index');
     Route::get('projects/{id}/database/tables', 'DatabaseController@index');
     Route::get('projects/{id}/database/tables/{table_name}', 'DatabaseController@show');
     Route::get('projects/{id}/preferences', 'ProjectPreferenceController@show');

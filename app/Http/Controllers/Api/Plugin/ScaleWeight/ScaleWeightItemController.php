54170b4d201fa9ca14b59392c0c6213836166ceb
Martien Dermawan Tanama <martiendt@gmail.com>
2019-08-23 10:27:02 +0700

    add update and delete endpoint

--- app/Http/Controllers/Api/Plugin/ScaleWeight/ScaleWeightItemController.php
+++ app/Http/Controllers/Api/Plugin/ScaleWeight/ScaleWeightItemController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api\Plugin\ScaleWeight;
 
+use App\Http\Resources\ApiResource;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Model\Plugin\ScaleWeight\ScaleWeightItem;
@@ -31,8 +32,7 @@ class ScaleWeightItemController extends Controller
      * Store a newly created resource in storage.
      *
      * @param \App\Http\Requests\Plugin\ScaleWeight\ScaleWeightItem\StoreScaleWeightItemRequest $request
-     *
-     * @return \App\Http\Resources\Plugin\ScaleWeight\ScaleWeightItem\ScaleWeightItemResource
+     * @return ApiResource
      */
     public function store(StoreScaleWeightItemRequest $request)
     {
@@ -50,7 +50,7 @@ class ScaleWeightItemController extends Controller
         $scaleWeightItem->net_weight = $request->get('net_weight');
         $scaleWeightItem->save();
 
-        return new ScaleWeightItemResource($scaleWeightItem);
+        return new ApiResource($scaleWeightItem);
     }
 
     /**
@@ -69,11 +69,25 @@ class ScaleWeightItemController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return ApiResource
      */
     public function update(Request $request, $id)
     {
-        //
+        $scaleWeightItem = ScaleWeightItem::findOrFail($id);
+
+        $scaleWeightItem->license_number = $request->get('license_number');
+        $scaleWeightItem->driver = $request->get('driver');
+        $scaleWeightItem->machine_code = $request->get('machine_code');
+        $scaleWeightItem->user = $request->get('user');
+        $scaleWeightItem->vendor = $request->get('vendor');
+        $scaleWeightItem->item = $request->get('item');
+        $scaleWeightItem->time = $request->get('time');
+        $scaleWeightItem->gross_weight = $request->get('gross_weight');
+        $scaleWeightItem->tare_weight = $request->get('tare_weight');
+        $scaleWeightItem->net_weight = $request->get('net_weight');
+        $scaleWeightItem->save();
+
+        return new ApiResource($scaleWeightItem);
     }
 
     /**
@@ -84,6 +98,9 @@ class ScaleWeightItemController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $scaleWeightItem = ScaleWeightItem::findOrFail($id);
+        $scaleWeightItem->delete();
+        
+        return response()->json([], 204);
     }
 }

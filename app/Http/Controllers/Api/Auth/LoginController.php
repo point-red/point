873d5adc923d4f5b895e7d6375d34d895796fd0a
Martien Dermawan Tanama <martiendt@gmail.com>
2019-11-13 15:27:30 +0700

    device detection on login

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -5,16 +5,18 @@ namespace App\Http\Controllers\Api\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Model\Project\Project;
+use Illuminate\Http\Response;
 use Illuminate\Support\Str;
+use Laravel\Passport\Token;
 
 class LoginController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @param \App\Http\Requests\Auth\LoginRequest $request
+     * @param LoginRequest $request
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index(LoginRequest $request)
     {
@@ -36,6 +38,17 @@ class LoginController extends Controller
         $user = auth()->guard('web')->user();
 
         $tokenResult = $user->createToken($user->name);
+
+        $tokenResult->token->is_mobile = $request->input('is_mobile') ?? null;
+        $tokenResult->token->os_name = $request->input('os_name') ?? null;
+        $tokenResult->token->os_version = $request->input('os_version') ?? null;
+        $tokenResult->token->browser_name = $request->input('browser_name') ?? null;
+        $tokenResult->token->browser_version = $request->input('browser_version') ?? null;
+        $tokenResult->token->mobile_vendor = $request->input('mobile_vendor') ?? null;
+        $tokenResult->token->mobile_model = $request->input('mobile_model') ?? null;
+        $tokenResult->token->engine_name = $request->input('engine_name') ?? null;
+        $tokenResult->token->engine_version = $request->input('engine_version') ?? null;
+        $tokenResult->token->save();
 
         $response = $user;
         $response->access_token = $tokenResult->accessToken;

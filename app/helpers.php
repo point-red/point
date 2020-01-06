f3203d7518f81c507b44f66fbba56b52db4f6ea9
Martien Dermawan Tanama <martiendt@gmail.com>
2020-01-06 09:42:33 +0700

    add helper converter

--- app/helpers.php
+++ app/helpers.php
@@ -302,3 +302,30 @@ if (! function_exists('convert_javascript_object_to_array')) {
         return $values;
     }
 }
+
+if (! function_exists('base64_to_jpeg')) {
+    /**
+     * convert base64 to jpg file
+     *
+     * @param $base64_string
+     * @param $output_file
+     * @return array
+     */
+    function base64_to_jpeg($base64_string, $output_file) {
+        // open the output file for writing
+        $ifp = fopen($output_file,'wb');
+
+        // split the string on commas
+        // $data[ 0 ] == "data:image/png;base64"
+        // $data[ 1 ] == <actual base64 string>
+        $data = explode(',', $base64_string);
+
+        // we could add validation here with ensuring count( $data ) > 1
+        fwrite($ifp, base64_decode($data[1]));
+
+        // clean up the file resource
+        fclose($ifp);
+
+        return $output_file;
+    }
+}

498832e3851b6930e219073a15d09bc3a2dfa9e5
Martien Dermawan Tanama <martiendt@gmail.com>
2020-02-26 14:54:11 +0700

    fix migration

--- database/migrations/tenant/2019_01_01_120003_create_item_groups_table.php
+++ database/migrations/tenant/2019_01_01_120003_create_item_groups_table.php
@@ -14,7 +14,7 @@ class CreateItemGroupsTable extends Migration
     public function up()
     {
         Schema::create('item_groups', function (Blueprint $table) {
-            $table->increments('id');ß
+            $table->increments('id');
             $table->string('name')->unique();
             $table->string('type')->nullable();
             $table->unsignedInteger('created_by')->index()->nullable();

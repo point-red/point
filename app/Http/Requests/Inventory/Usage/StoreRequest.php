dedfe5491a1678481f47e619dffc5b417e8ddf35
Martien Dermawan Tanama <martiendt@gmail.com>
2020-03-11 16:25:17 +0700

    fix request validation

--- app/Http/Requests/Inventory/Usage/StoreRequest.php
+++ app/Http/Requests/Inventory/Usage/StoreRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Inventory\Usage;
 
+use App\Http\Requests\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreRequest extends FormRequest
@@ -26,6 +27,12 @@ class StoreRequest extends FormRequest
         return [
             'warehouse_id' => 'required',
             'request_approval_to' => 'required',
+
+            'items.*.item_id' => ValidationRule::foreignKey('items'),
+            'items.*.quantity' => ValidationRule::quantity(),
+            'items.*.unit' => ValidationRule::unit(),
+            'items.*.converter' => ValidationRule::converter(),
+            'items.*.chart_of_account_id' => 'required',
         ];
     }
 }

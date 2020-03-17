46b1ac71b839825a9b53a28a31050b80b869e749
Martien Dermawan Tanama <martiendt@gmail.com>
2020-03-17 14:15:58 +0700

    fix

--- routes/api/project.php
+++ routes/api/project.php
@@ -1,6 +1,7 @@
 <?php
 
 Route::prefix('project')->namespace('Project')->group(function () {
+    Route::delete('project-users', 'ProjectUserController@destroy');
     Route::apiResource('projects', 'ProjectController');
     Route::get('projects/{id}/database/backups', 'DatabaseBackupController@index');
     Route::post('projects/{id}/database/backups', 'DatabaseBackupController@store');

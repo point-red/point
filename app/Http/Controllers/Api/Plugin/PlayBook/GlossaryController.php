6b62cd9e9a1a01e0b244fc15b7d99dd0d7fa7119
Ibnu Jakaria <ibnujakaria@outlook.com>
2020-04-06 13:52:49 +0700

    delete glossary

--- app/Http/Controllers/Api/Plugin/PlayBook/GlossaryController.php
+++ app/Http/Controllers/Api/Plugin/PlayBook/GlossaryController.php
@@ -91,8 +91,10 @@ class GlossaryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Glossary $glossary)
     {
-        //
+        $glossary->delete();
+
+        return ['message' => 'Glossary deleted.'];
     }
 }

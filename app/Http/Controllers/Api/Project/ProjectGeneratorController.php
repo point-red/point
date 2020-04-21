5142aa00be89779c8fc5fed97d79852fa46ad57b
Martien Dermawan Tanama <martiendt@gmail.com>
2020-04-21 09:11:38 +0700

    fix class name

--- app/Http/Controllers/Api/Project/ProjectGeneratorController.php
+++ app/Http/Controllers/Api/Project/ProjectGeneratorController.php
@@ -16,32 +16,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 
-class ProjectController extends Controller
+class ProjectGeneratorController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @return ApiCollection
-     */
-    public function index(Request $request)
-    {
-        $projects = Project::join('project_user', 'projects.id', '=', 'project_user.project_id')
-            ->where('project_user.user_id', auth()->user()->id)
-            ->select('projects.*', 'user_id', 'user_name', 'user_email', 'joined', 'request_join_at', 'project_user.id as user_invitation_id');
-
-        if ($request->get('search')) {
-            $projects = $projects->where(function ($q) use ($request) {
-                $q->where('code', 'like', '%'.$request->get('search').'%')
-                    ->orWhere('name', 'like', '%'.$request->get('search').'%');
-            });
-        }
-
-        $projects = pagination($projects, $request->input('limit'));
-
-        return new ApiCollection($projects);
-    }
-
     /**
      * Store a newly created resource in storage.
      *
@@ -52,7 +28,7 @@ class ProjectController extends Controller
     {
         // User only allowed to create max 1 project
         $numberOfProject = Project::where('owner_id', auth()->user()->id)->count();
-        // TODO: disable new project creation
+            // TODO: disable new project creation
         if ($numberOfProject >= 100) {
             return response()->json([
                 'code' => 422,
@@ -108,72 +84,6 @@ class ProjectController extends Controller
         Artisan::call('tenant:seed:first', ['db_name' => $dbName]);
 
         DB::connection('tenant')->commit();
-
-        return new ProjectResource($project);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int $id
-     * @return ApiResource
-     */
-    public function show($id)
-    {
-        $project = Project::findOrFail($id)->load('users');
-
-        $dbName = env('DB_DATABASE').'_'.strtolower($project->code);
-
-        $project->db_size = dbm_get_size($dbName, 'tenant');
-
-        return new ApiResource($project);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \App\Http\Requests\Project\Project\UpdateProjectRequest $request
-     * @param  int                                                    $id
-     *
-     * @return \App\Http\Resources\Project\Project\ProjectResource
-     */
-    public function update(UpdateProjectRequest $request, $id)
-    {
-        // Update tenant database name in configuration
-        $project = Project::findOrFail($id);
-        $project->name = $request->get('name');
-        $project->group = $request->get('group');
-        $project->address = $request->get('address');
-        $project->phone = $request->get('phone');
-        $project->whatsapp = $request->get('whatsapp');
-        $project->website = $request->get('website');
-        $project->marketplace_notes = $request->get('marketplace_notes');
-        $project->vat_id_number = $request->get('vat_id_number');
-        $project->invitation_code = $request->get('invitation_code');
-        $project->invitation_code_enabled = $request->get('invitation_code_enabled');
-        $project->save();
-
-        return new ProjectResource($project);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param \App\Http\Requests\Project\Project\DeleteProjectRequest $request
-     * @param  int                                                    $id
-     *
-     * @return \App\Http\Resources\Project\Project\ProjectResource
-     */
-    public function destroy(DeleteProjectRequest $request, $id)
-    {
-        $project = Project::findOrFail($id);
-
-        $project->delete();
-
-        // Delete database tenant
-        Artisan::call('tenant:database:delete', [
-            'db_name' => env('DB_DATABASE').'_'.strtolower($project->code),
-        ]);
 
         return new ProjectResource($project);
     }

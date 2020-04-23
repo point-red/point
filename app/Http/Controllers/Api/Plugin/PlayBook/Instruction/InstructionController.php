6306a1a3b8380d5db0d6d0c6fb8dcee5f13a0817
Ibnu Jakaria <ibnujakaria@outlook.com>
2020-04-23 15:42:59 +0700

    delete instruction

--- app/Http/Controllers/Api/Plugin/PlayBook/Instruction/InstructionController.php
+++ app/Http/Controllers/Api/Plugin/PlayBook/Instruction/InstructionController.php
@@ -78,8 +78,12 @@ class InstructionController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Instruction $instruction)
     {
-        //
+        $instruction->delete();
+
+        return response()->json([
+            'message' => 'Deleted.'
+        ]);
     }
 }

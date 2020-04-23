d632ebfdeca32849d2fe119e8df8af93b410d79d
Ibnu Jakaria <ibnujakaria@outlook.com>
2020-04-23 12:25:45 +0700

    hapus step

--- app/Http/Controllers/Api/Plugin/PlayBook/Instruction/StepController.php
+++ app/Http/Controllers/Api/Plugin/PlayBook/Instruction/StepController.php
@@ -116,8 +116,9 @@ class StepController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Instruction $instruction, InstructionStep $step)
     {
-        //
+        $step->contents()->delete();
+        $step->delete();
     }
 }

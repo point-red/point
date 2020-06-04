9be849e0b94fc1d507f9eb7a60a28df71f884605
DESKTOP-M02T53A\Timothy <timothypp@bangjati.com>
2020-06-04 11:03:11 +0700

    Bug Fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Horizon\Horizon;
 use Psr\Log\LoggerInterface;
@@ -17,6 +18,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
         Horizon::auth(function () {
             return true;
         });

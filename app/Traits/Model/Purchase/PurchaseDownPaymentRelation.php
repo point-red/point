697685dd61f79cd56f8c96ab8648a7206215a896
Christhofer Natalius <christhofernatalius@gmail.com>
2020-06-05 17:04:16 +0700

    fixed purchase down payment relation

--- app/Traits/Model/Purchase/PurchaseDownPaymentRelation.php
+++ app/Traits/Model/Purchase/PurchaseDownPaymentRelation.php
@@ -32,11 +32,29 @@ trait PurchaseDownPaymentRelation
      */
     public function payments()
     {
-        return $this->morphToMany(Payment::class, 'referenceable', 'payment_details')->active();
+        return $this->morphToMany(Payment::class, 'referenceable', 'payment_details')
+            ->join(Form::getTableName(), function ($q) {
+                $q->on(Form::getTableName('formable_id'), '=', Payment::getTableName('id'))
+                    ->where(Form::getTableName('formable_type'), Payment::$morphName);
+            })
+            ->whereNotNull(Form::getTableName('number'))
+            ->where(function ($q) {
+                $q->whereNull(Form::getTableName('cancellation_status'))
+                    ->orWhere(Form::getTableName('cancellation_status'), '!=', '1');
+            });
     }
 
     public function invoices()
     {
-        return $this->belongsToMany(PurchaseInvoice::class, 'purchase_down_payment_invoice', 'invoice_id', 'down_payment_id')->active();
+        return $this->belongsToMany(PurchaseInvoice::class, 'purchase_down_payment_invoice', 'invoice_id', 'down_payment_id')
+            ->join(Form::getTableName(), function ($q) {
+                $q->on(Form::getTableName('formable_id'), '=', PurchaseInvoice::getTableName('id'))
+                    ->where(Form::getTableName('formable_type'), PurchaseInvoice::$morphName);
+            })
+            ->whereNotNull(Form::getTableName('number'))
+            ->where(function ($q) {
+                $q->whereNull(Form::getTableName('cancellation_status'))
+                    ->orWhere(Form::getTableName('cancellation_status'), '!=', '1');
+            });
     }
 }

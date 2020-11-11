089fa3006d0e377cd6a01c030d7fd47fc8c3cc79
Martien Dermawan Tanama <martiendt@gmail.com>
2020-11-11 12:21:02 +0700

    check data

--- app/Console/Commands/AlterData.php
+++ app/Console/Commands/AlterData.php
@@ -2,33 +2,14 @@
 
 namespace App\Console\Commands;
 
-use App\Imports\Template\ChartOfAccountImport;
-use App\Imports\Template\ItemImport;
-use App\Model\Accounting\ChartOfAccount;
-use App\Model\Accounting\ChartOfAccountType;
-use App\Model\Accounting\CutOff;
-use App\Model\Finance\PaymentOrder\PaymentOrder;
-use App\Model\HumanResource\Employee\Employee\EmployeeAddress;
-use App\Model\HumanResource\Employee\Employee\EmployeePhone;
-use App\Model\HumanResource\Employee\EmployeeEmail;
-use App\Model\Manufacture\ManufactureFormula\ManufactureFormula;
-use App\Model\Master\Address;
-use App\Model\Master\Branch;
-use App\Model\Master\Customer;
-use App\Model\Master\Email;
-use App\Model\Master\Item;
-use App\Model\Master\ItemUnit;
-use App\Model\Master\Phone;
-use App\Model\Master\PricingGroup;
-use App\Model\Master\User;
-use App\Model\Master\Warehouse;
+use App\Model\Inventory\Inventory;
+use App\Model\Plugin\PinPoint\SalesVisitation;
 use App\Model\Project\Project;
-use App\Model\Purchase\PurchaseRequest\PurchaseRequest;
-use App\Model\SettingJournal;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Facades\Excel;
+use PhpParser\Node\Scalar\MagicConst\Line;
 
 class AlterData extends Command
 {
@@ -66,205 +47,25 @@ class AlterData extends Command
         $projects = Project::where('is_generated', true)->get();
         foreach ($projects as $project) {
             $this->line('Clone '.$project->code);
-            Artisan::call('tenant:database:backup-clone', ['project_code' => strtolower($project->code)]);
+            // Artisan::call('tenant:database:backup-clone', ['project_code' => strtolower($project->code)]);
 
             $this->line('Alter '.$project->code);
             config()->set('database.connections.tenant.database', env('DB_DATABASE').'_'.strtolower($project->code));
-
+            
             DB::connection('tenant')->reconnect();
             DB::connection('tenant')->beginTransaction();
+            
 
-            $eAddressess = EmployeeAddress::all();
-            foreach ($eAddressess as $eAddress) {
-                $address = Address::where('addressable_type', 'Employeee')
-                ->where('addressable_id', $eAddress->employee_id)
-                ->first();
+            $salesVisitations = SalesVisitation::where('payment_method', 'sell-out')->orWhere('payment_method', 'taking-order')->get();
 
-                if (!$address) {
-                    $address = new Address;
-                    $address->address = $eAddress->address;
-                    $address->addressable_type = 'Employee';
-                    $address->addressable_id = $eAddress->employee_id;
-                    $address->save();
+            foreach ($salesVisitations as $salesVisitation) {
+                $inventory = Inventory::where('form_id', $salesVisitation->form->number)->first();
+                if ($inventory) {
+                    $this->line($salesVisitation->form->number);
                 }
-            }
-
-            $ePhones = EmployeePhone::all();
-            foreach ($ePhones as $ePhone) {
-                $phone = Phone::where('phoneable_type', 'Employeee')
-                ->where('phoneable_id', $ePhone->employee_id)
-                ->first();
-                
-                if (!$phone) {
-                    $phone = new Phone;
-                    $phone->number = $ePhone->phone;
-                    $phone->phoneable_type = 'Employee';
-                    $phone->phoneable_id = $ePhone->employee_id;
-                    $phone->save();
-                }
-            }
-
-            $eEmails = EmployeeEmail::all();
-            foreach ($eEmails as $eEmail) {
-                $email = Email::where('emailable_type', 'Employeee')
-                ->where('emailable_id', $eEmail->employee_id)
-                ->first();
-                
-                if (!$email) {
-                    $email = new Email;
-                    $email->email = $eEmail->email;
-                    $email->emailable_type = 'Employee';
-                    $email->emailable_id = $eEmail->employee_id;
-                    $email->save();
-                }
-            }
-
-            // $addresses = Address::all();
-            // foreach ($addresses as $address) {
-            //     if (!$address->addressable) {
-            //         $address->delete();
-            //     } else {
-            //         $address->addressable->address = $address->address;
-            //         $address->addressable->save();
-            //     }
-            // }
-
-            // $phones = Phone::all();
-            // foreach ($phones as $phone) {
-            //     if (!$phone->phoneable) {
-            //         $phone->delete();
-            //     } else {
-            //         $phone->phoneable->phone = $phone->number;
-            //         $phone->phoneable->save();
-            //     }
-            // }
-
-            // $emails = Email::all();
-            // foreach ($emails as $email) {
-            //     if (!$email->emailable) {
-            //         $email->delete();
-            //     } else {
-            //         $email->emailable->email = $email->email;
-            //         $email->emailable->save();
-            //     }
-            // }
-
-            // Excel::import(new ChartOfAccountImport(), storage_path('template/chart_of_accounts_manufacture.xlsx'));
-            // Excel::import(new ItemImport(), storage_path('template/items.xls'));
-
-//            $this->setData();
-
-//            SettingJournal::query()->truncate();
-//            ChartOfAccount::query()->truncate();
-//            ChartOfAccountType::query()->truncate();
-//
-//            Artisan::call('db:seed', [
-//                '--database' => 'tenant',
-//                '--class' => 'ChartOfAccountTypeSeeder',
-//                '--force' => true,
-//            ]);
-//
-//            Excel::import(new ChartOfAccountImport(), storage_path('template/chart_of_accounts_manufacture.xlsx'));
-//
-//            Artisan::call('db:seed', [
-//                '--database' => 'tenant',
-//                '--class' => 'SettingJournalSeeder',
-//                '--force' => true,
-//            ]);
-//
-//            $items = Item::all();
-//            $account = ChartOfAccount::where('alias', 'PERSEDIAAN BAHAN BAKU')->first();
-//
-//            foreach ($items as $item) {
-//                $item->chart_of_account_id = $account->id;
-//                $item->save();
-//
-//                if ($item->unit_default == null || $item->unit_default_purchase == null || $item->unit_default_sales == null) {
-//                    $unit = ItemUnit::where('item_id', $item->id)->first();
-//                    if (!$unit) {
-//                        $unit = new ItemUnit;
-//                        $unit->label = 'PCS';
-//                        $unit->name = 'PCS';
-//                        $unit->converter = 1;
-//                        $unit->save();
-//                    }
-//
-//                    $item->unit_default = $unit->id;
-//                    $item->unit_default_purchase = $unit->id;
-//                    $item->unit_default_sales = $unit->id;
-//                    $item->save();
-//                }
-//            }
-//
+            } 
+            
             DB::connection('tenant')->commit();
-        }
-    }
-
-    private function setData()
-    {
-        $formulas = ManufactureFormula::all();
-        foreach ($formulas as $formula) {
-            if ($formula->form->request_approval_to == null) {
-                $formula->form->request_approval_to = User::first()->id;
-                $formula->form->save();
-            }
-        }
-
-        $cutOffs = CutOff::all();
-        foreach ($cutOffs as $cutOff) {
-            if ($cutOff->form->request_approval_to == null) {
-                $cutOff->form->request_approval_to = User::first()->id;
-                $cutOff->form->save();
-            }
-        }
-
-        $purchaseRequests = PurchaseRequest::all();
-        foreach ($purchaseRequests as $purchaseRequest) {
-            if ($purchaseRequest->form->request_approval_to == null) {
-                $purchaseRequest->form->request_approval_to = User::first()->id;
-                $purchaseRequest->form->save();
-            }
-        }
-
-        if (PricingGroup::all()->count() == 0) {
-            $pricingGroup = new PricingGroup;
-            $pricingGroup->label = 'DEFAULT';
-            $pricingGroup->save();
-        }
-
-        if (Branch::all()->count() == 0) {
-            $branch = new Branch;
-        } else {
-            $branch = Branch::find(1);
-        }
-
-        $branch->name = 'CENTRAL';
-        $branch->save();
-
-        $users = User::all();
-
-        DB::connection('tenant')->table('branch_user')->update([
-            'is_default' => false,
-        ]);
-
-        foreach ($users as $user) {
-            $user->branches()->detach(1);
-            $user->branches()->attach(1, ['is_default' => 1]);
-        }
-
-        if (Warehouse::all()->count() == 0) {
-            $warehouse = new Warehouse;
-        } else {
-            $warehouse = Warehouse::find(1);
-        }
-
-        $warehouse->branch_id = $branch->id;
-        $warehouse->name = 'MAIN WAREHOUSE';
-        $warehouse->save();
-
-        foreach (Warehouse::all() as $warehouse) {
-            $warehouse->branch_id = $branch->id;
-            $warehouse->save();
         }
     }
 }

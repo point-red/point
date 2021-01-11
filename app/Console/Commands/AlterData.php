a3f546960af1f2df5e53b046bfdfe5b03751a76b
Martien Dermawan Tanama <martiendt@gmail.com>
2021-01-11 13:50:52 +0700

    bump

--- app/Console/Commands/AlterData.php
+++ app/Console/Commands/AlterData.php
@@ -3,13 +3,9 @@
 namespace App\Console\Commands;
 
 use App\Model\Inventory\Inventory;
-use App\Model\Inventory\InventoryUsage\InventoryUsage;
-use App\Model\Inventory\InventoryUsage\InventoryUsageItem;
-use App\Model\Plugin\PinPoint\SalesVisitation;
 use App\Model\Project\Project;
 use App\Model\Purchase\PurchaseInvoice\PurchaseInvoice;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 
 class AlterData extends Command
@@ -48,25 +44,23 @@ class AlterData extends Command
         $projects = Project::where('is_generated', true)->get();
         foreach ($projects as $project) {
             $this->line('Clone '.$project->code);
-            Artisan::call('tenant:database:backup-clone', ['project_code' => strtolower($project->code)]);
+            // Artisan::call('tenant:database:backup-clone', ['project_code' => strtolower($project->code)]);
 
             $this->line('Alter '.$project->code);
             config()->set('database.connections.tenant.database', env('DB_DATABASE').'_'.strtolower($project->code));
             
             DB::connection('tenant')->reconnect();
             DB::connection('tenant')->beginTransaction();
-
-            InventoryUsageItem::where('quantity', '=', 0)->delete();
             
-            // $invoices = SalesVisitation::all();
+            $invoices = PurchaseInvoice::all();
 
-            // foreach($invoices as $invoice) {
-            //     $aCount = count($invoice->details);
-            //     $bCount = Inventory::where('form_id', '=', $invoice->form->id)->count();
-            //     if ($invoice->form->cancellation_approval_at === null && $aCount < $bCount) {
-            //         $this->line($invoice->form->number . ' : '. $aCount . ' = ' . $bCount . ' @' . $invoice->form->createdBy->name);
-            //     }
-            // }
+            foreach($invoices as $invoice) {
+                $aCount = count($invoice->items);
+                $bCount = Inventory::where('form_id', '=', $invoice->form->id)->count();
+                if ($invoice->form->cancellation_approval_at === null && $aCount < $bCount) {
+                    $this->line($invoice->form->number . ' : '. $aCount . ' = ' . $bCount . ' @' . $invoice->form->createdBy->name);
+                }
+            }
 
             DB::connection('tenant')->commit();
         }

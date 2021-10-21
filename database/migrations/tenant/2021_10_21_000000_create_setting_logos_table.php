78d0ea5b2404ec8cb0a02560c4a120829da96ac7
Martien Dermawan Tanama <martiendt@gmail.com>
2021-10-21 10:58:54 +0700

    fix class name

--- database/migrations/tenant/2021_10_21_000000_create_setting_logos_table.php
+++ database/migrations/tenant/2021_10_21_000000_create_setting_logos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSettingLogoTable extends Migration
+class CreateSettingLogosTable extends Migration
 {
     /**
      * Run the migrations.

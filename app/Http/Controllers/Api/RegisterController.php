6b48c7b13ace7565115ac59cfc08f10e439604d0
satriyoaji <satriyoaji27@gmail.com>
2021-12-31 12:50:37 +0700

    add update project user_id of registered user

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\Master\User\StoreUserRequest;
 use App\Http\Resources\ApiResource;
+use App\Model\Project\ProjectUser;
 use App\User;
+use Illuminate\Support\Facades\DB;
 
 class RegisterController extends ApiController
 {
     public function store(StoreUserRequest $request)
     {
+        DB::beginTransaction();
         $emailConfirmationCode = substr(encrypt($request->input('email')), 0, 30);
 
         $user = new User;
@@ -23,6 +26,11 @@ class RegisterController extends ApiController
         $user->phone = $request->phone;
         $user->save();
 
+        $project = ProjectUser::where('user_email', $user->email)->first();
+        $project->user_id = $user->id;
+        $project->save();
+
+        DB::commit();
         return new ApiResource($user);
     }
 }

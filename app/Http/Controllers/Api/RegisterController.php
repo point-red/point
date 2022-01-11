26793aaca8ee2f29551a0d8794efccda25669ccb
Martien Dermawan Tanama <martiendt@gmail.com>
2022-01-11 13:41:43 +0700

    fix invitation for unregistered user

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers\Api;
 use App\Http\Requests\Master\User\StoreUserRequest;
 use App\Http\Resources\ApiResource;
 use App\User;
+use Illuminate\Support\Facades\DB;
+use App\Model\Project\ProjectUser;
 
 class RegisterController extends ApiController
 {
     public function store(StoreUserRequest $request)
     {
         $emailConfirmationCode = substr(encrypt($request->input('email')), 0, 30);
+
+        DB::beginTransaction();
 
         $user = new User;
         $user->name = $request->username;
@@ -22,6 +26,14 @@ class RegisterController extends ApiController
         $user->address = $request->address;
         $user->phone = $request->phone;
         $user->save();
+
+        $projects = ProjectUser::where('user_email', $user->email)->get();
+        foreach($projects as $project) {
+            $project->user_id = $user->id;
+            $project->save();
+        }
+
+        DB::commit();
 
         return new ApiResource($user);
     }

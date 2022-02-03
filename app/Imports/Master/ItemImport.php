a6ae8e4fb09c1dfbfb260fba1dacafc9f9276636
Bayu Ramadhan <email.bayu.ramadhan@gmail.com>
2022-02-03 12:39:08 +0700

    update import master item

--- app/Imports/Master/ItemImport.php
+++ app/Imports/Master/ItemImport.php
@@ -24,35 +24,30 @@ class ItemImport implements ToCollection
       if($index <= $this->start_row)  {
         continue;
       }
-      try{
-        if ($row[request()->get("name")] !== 'null' && $row[request()->get("code")] !== 'null' && $row[request()->get("chart_of_account")] !== 'null') {
-            //set item
-            $item = $this->generateItem($row);    
-            //check chart of account
-            $accounts = ChartOfAccount::select('id')->where('alias', strtoupper($item['chart_of_account']))->first();
-            if(isset($accounts->id)){
-                $item['chart_of_account_id'] = $accounts->id;
-                  if(isset($item['group_name'])){
-                      $itemGroup = ItemGroup::select('id','name')->where('name', $item['group_name'])->get();
-                      if($itemGroup->isEmpty()){
-                          $itemGroup = new ItemGroup();
-                          $itemGroup->name = $item['group_name'];
-                          $itemGroup->save();
-                          $itemGroup = [$itemGroup];
-                      }
-                      $item['groups'] = $itemGroup;
-                  }
-                  $save = Item::create($item);
-                  $this->success++;
-            }else{
-                $this->fail++;
-            }
-        }else{
-          $this->fail++; 
-        }
-      }catch(\Exception $e){
+      if ($row[request()->get("name")] != null && $row[request()->get("code")] != null && $row[request()->get("chart_of_account")] != null) {
+          //set item
+          $item = $this->generateItem($row);    
+          //check chart of account
+          $accounts = ChartOfAccount::select('id')->where('alias', strtoupper($item['chart_of_account']))->first();
+          if(isset($accounts->id)){
+              $item['chart_of_account_id'] = $accounts->id;
+                if(isset($item['group_name'])){
+                    $itemGroup = ItemGroup::select('id','name')->where('name', $item['group_name'])->get();
+                    if($itemGroup->isEmpty()){
+                        $itemGroup = new ItemGroup();
+                        $itemGroup->name = $item['group_name'];
+                        $itemGroup->save();
+                        $itemGroup = [$itemGroup];
+                    }
+                    $item['groups'] = $itemGroup;
+                }
+                $save = Item::create($item);
+                $this->success++;
+          }else{
+              $this->fail++;
+          }
+      }else{
         $this->fail++; 
-        $this->exception = $e->getMessage();
       }
     }
   }
@@ -63,26 +58,26 @@ class ItemImport implements ToCollection
     $item['name'] = $row[request()->get("name")];
     $item['chart_of_account'] = $row[request()->get("chart_of_account")];
     $item['units'] = [];
-    if(request()->get("units_converter_1") != 'null' && request()->get("units_measurement_1") != 'null'){
-        if($row[request()->get("units_converter_1")] != 'null' && $row[request()->get("units_measurement_1")] != 'null'){
+    if(request()->get("units_converter_1") != null && request()->get("units_measurement_1") != null){
+        if($row[request()->get("units_converter_1")] != null && $row[request()->get("units_measurement_1")] != null){
             array_push($item['units'], $this->generateUnits($row[request()->get("units_converter_1")],$row[request()->get("units_measurement_1")]));
         }
     }
-    if(request()->get("units_converter_2") != 'null' && request()->get("units_measurement_2") != 'null'){
-      if($row[request()->get("units_converter_2")] != 'null' && $row[request()->get("units_measurement_2")] != 'null'){
+    if(request()->get("units_converter_2") != null && request()->get("units_measurement_2") != null){
+      if($row[request()->get("units_converter_2")] != null && $row[request()->get("units_measurement_2")] != null){
           array_push($item['units'], $this->generateUnits($row[request()->get("units_converter_2")],$row[request()->get("units_measurement_2")]));
       }
     }
     if(empty($item['units'])){
       array_push($item['units'], $this->generateUnits('pcs', 1));
     }
-    if(request()->get("require_expiry_date") !== 'null' && $row[request()->get("require_expiry_date")]!== 'null'){
-      $item['require_expiry_date'] = strtolower($row[request()->get("require_expiry_date")]) == 'true' ? true : false;
+    if(request()->get("require_expiry_date") != null && $row[request()->get("require_expiry_date")]!= null){
+      $item['require_expiry_date'] = $row[request()->get("require_expiry_date")];
     }
-    if(request()->get("require_production_number") !== 'null' && $row[request()->get("require_production_number")]!== 'null'){
+    if(request()->get("require_production_number") != null && $row[request()->get("require_production_number")]!= null){
       $item['require_production_number'] = $row[request()->get("require_production_number")];
     }
-    if(request()->get("group_name") !== 'null' && $row[request()->get("group_name")]!== 'null'){
+    if(request()->get("group_name") != null && $row[request()->get("group_name")]!= null){
       $item['group_name'] = $row[request()->get("group_name")];
     }
     return $item;

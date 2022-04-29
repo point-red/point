7d35f0f0825bb7f454f43b3d295676e4eed33de9
jackson <ramadhansalmanalfarisi8@gmail.com>
2022-04-29 14:50:09 +0700

    revert export customer

--- tests/Feature/Http/Master/CustomerTest.php
+++ tests/Feature/Http/Master/CustomerTest.php
@@ -31,37 +31,4 @@ class CustomerTest extends TestCase
             'name' => $data['name'],
         ], 'tenant');
     }
-
-    /** @test */
-    public function user_can_download_customers_export_test()
-    {
-        $datetimenow = date("Y-m-d H:i:s");
-        $data = array(
-            'datetimenow' => $datetimenow,
-            'group_id' => '1',
-            'join' => 'address,phone,email'
-        );
-        $customer = new Customer();
-        $customer->name = "Ramadhan";
-        $customer->branch_id = $this->branch_id;
-        $customer->save();
-
-        $customerGroup = new CustomerGroup();
-        $customerGroup->name = "Group1";
-        $customerGroup->save();
-
-        DB::connection('tenant')->table('customer_customer_group')->insert([
-            'customer_id' => $customer->id,
-            'customer_group_id' => $customerGroup->id,
-            'created_at' => date("Y-m-d H:i:s")
-        ]);
-
-        $response = $this->json('POST', '/api/v1/master/customers/export', $data, $this->headers);
-        $response->assertStatus(200);
-        $nameUser = $this->user->name;
-        $dateNow = date("Ymdhis", strtotime($datetimenow));
-        $nameFile = $nameUser . '-' . $dateNow . '-customers.xlsx';
-        $this->assertTrue($response->headers->get('Content-Type') == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        $this->assertTrue($response->headers->get('Content-Disposition') == 'attachment; filename="' . $nameFile . '"');
-    }
 }

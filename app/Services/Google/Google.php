d1f5310667d084defe90f41fd23dc656e671fc23
Martien Dermawan Tanama <martiendt@gmail.com>
2022-07-21 16:48:43 +0700

    fix env

--- app/Services/Google/Google.php
+++ app/Services/Google/Google.php
@@ -23,7 +23,7 @@ class Google
             "auth_uri" => "https://accounts.google.com/o/oauth2/auth",
             "token_uri" => "https://oauth2.googleapis.com/token",
             "auth_provider_x509_cert_url" => "https://www.googleapis.com/oauth2/v1/certs",
-            "project_id" => "point-test-gdrive",
+            "project_id" => config('services.google.project_id'),
             "client_id" => $oauthClientId,
             "client_secret" => $oauthClientSecret
         ]);

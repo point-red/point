0ee4717f476ee0991a4c63d4eb49fb4fd7489fd3
Christhofer <christhofer@gradin.co.id>
2022-07-21 19:47:09 +0700

    fixed required if validation if not draft

--- app/Http/Requests/Plugin/Study/StudySheetStoreRequest.php
+++ app/Http/Requests/Plugin/Study/StudySheetStoreRequest.php
@@ -24,21 +24,38 @@ class StudySheetStoreRequest extends FormRequest
     public function rules()
     {
         return [
-            'started_at' => 'required_if:is_draft,false',
-            'ended_at' => 'required_if:is_draft,false',
+            'started_at' => 'required_if:is_draft,0',
+            'ended_at' => 'required_if:is_draft,0',
             'photo' => 'nullable|image',
             'audio' => 'nullable|mimetypes:audio/*',
             'video' => 'nullable|mimetypes:video/*',
-            'subject_id' => 'required_if:is_draft,false|exists:tenant.study_subjects,id',
+            'subject_id' => 'required_if:is_draft,0|exists:tenant.study_subjects,id',
             'institution' => 'nullable|string|max:180',
             'teacher' => 'nullable|string|max:180',
-            'competency' => 'required_if:is_draft,false|string|max:180',
-            'learning_goals' => 'required_if:is_draft,false|string|max:180',
+            'competency' => 'required_if:is_draft,0|string|max:180',
+            'learning_goals' => 'required_if:is_draft,0|string|max:180',
             'activities' => 'nullable|string|max:180',
             'grade' => 'nullable|integer|max:100',
-            'behavior' => 'required_if:is_draft,false|string|max:1|in:A,B,C',
+            'behavior' => 'required_if:is_draft,0|string|max:1|in:A,B,C',
             'remarks' => 'nullable|string|max:180',
             'is_draft' => 'required|boolean',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'started_at.required_if' => __('validation.required'),
+            'ended_at.required_if' => __('validation.required'),
+            'subject_id.required_if' => __('validation.required'),
+            'competency.required_if' => __('validation.required'),
+            'learning_goals.required_if' => __('validation.required'),
+            'behavior.required_if' => __('validation.required'),
         ];
     }
 }

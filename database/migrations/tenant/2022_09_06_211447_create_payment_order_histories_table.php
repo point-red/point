350d2f3900bb2db1ca664af46961c49bbffa296f
M Adyan Rohutomo <muhadyan@Adyans-MacBook-Pro.local>
2022-09-14 22:13:34 +0700

    create migration for payment order

---
+++ database/migrations/tenant/2022_09_06_211447_create_payment_order_histories_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentOrderHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment_order_histories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('payment_order_id')->index();
+            $table->unsignedInteger('user_id')->index();
+            $table->string('activity')->nullable();
+            $table->timestamps();
+
+            $table->foreign('payment_order_id')
+                ->references('id')
+                ->on('payment_orders');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment_order_histories');
+    }
+}

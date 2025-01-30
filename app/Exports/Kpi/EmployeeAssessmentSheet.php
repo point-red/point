c1d864392debbb478e5195f0cf59acac57dad80f
Martien Dermawan Tanama <martiendt@gmail.com>
2025-01-30 08:25:30 +0700

    x

--- app/Exports/Kpi/EmployeeAssessmentSheet.php
+++ app/Exports/Kpi/EmployeeAssessmentSheet.php
@@ -7,6 +7,8 @@ use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithTitle;
 use Illuminate\Support\Facades\DB;
+use App\Http\Resources\HumanResource\Kpi\Kpi\KpiCollection;
+use App\Http\Resources\HumanResource\Kpi\Kpi\KpiResource;
 
 class EmployeeAssessmentSheet implements FromCollection, WithTitle, WithHeadings
 {
@@ -32,15 +34,35 @@ class EmployeeAssessmentSheet implements FromCollection, WithTitle, WithHeadings
             ->addSelect(DB::raw('sum(kpi_indicators.score) / count(DISTINCT kpis.id) as score'))
             ->addSelect(DB::raw('sum(kpi_indicators.score_percentage) / count(DISTINCT kpis.id) as score_percentage'))
             ->addSelect(DB::raw('count(DISTINCT kpis.id) as num_of_scorer'));
-
+        $kpis = $kpis->groupBy('kpis.id');
         $kpis = $kpis->where('employee_id', $this->employee_id)
         ->where('date', '>=',$this->dateFrom)
         ->where('date', '<=',$this->dateTo)
         ->orderBy('kpis.date', 'desc')->orderBy('kpis.created_at', 'desc')->get();
 
-        \Log::info($kpis);
+        $dates = [];
+        $scores = [];
 
-        return $kpis;
+        foreach ($kpis as $key => $kpi) {
+            array_push($dates, date('dMY', strtotime($kpi->date)));
+            array_push($scores, number_format($kpi->score_percentage, 2));
+        }
+
+        \Log::info( (new KpiCollection($kpis))
+        ->additional([
+            'data_set' => [
+                'dates' => $dates,
+                'scores' => $scores,
+            ],
+        ]));
+
+        return (new KpiCollection($kpis))
+            ->additional([
+                'data_set' => [
+                    'dates' => $dates,
+                    'scores' => $scores,
+                ],
+            ]);
     }
 
     public function headings(): array

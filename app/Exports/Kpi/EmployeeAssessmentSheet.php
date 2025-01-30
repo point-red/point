559218c0e4a23b099659aaae639b9488c370a48c
Martien Dermawan Tanama <martiendt@gmail.com>
2025-01-30 08:17:17 +0700

    x

--- app/Exports/Kpi/EmployeeAssessmentSheet.php
+++ app/Exports/Kpi/EmployeeAssessmentSheet.php
@@ -7,7 +7,7 @@ use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithTitle;
 
-class AssessmentAssessmentSheet implements FromCollection, WithTitle, WithHeadings
+class EmployeeAssessmentSheet implements FromCollection, WithTitle, WithHeadings
 {
     private $employee_id;
     private $dateFrom;

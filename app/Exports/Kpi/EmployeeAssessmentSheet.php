ed2e4e59235ce2b8bc65e28760ee9b8892c315e1
Martien Dermawan Tanama <martiendt@gmail.com>
2025-01-30 08:39:37 +0700

    x

--- app/Exports/Kpi/EmployeeAssessmentSheet.php
+++ app/Exports/Kpi/EmployeeAssessmentSheet.php
@@ -36,7 +36,7 @@ class EmployeeAssessmentSheet implements FromCollection, WithTitle, WithHeadings
             ->addSelect(DB::raw('count(DISTINCT kpis.id) as num_of_scorer'));
         
         $kpis = $kpis->where('employee_id', $this->employee_id)
-            ->group_by('kpis.id')
+            ->groupBy('kpis.id')
             ->where('kpis.date', '>=',$this->dateFrom)
             ->where('kpis.date', '<=',$this->dateTo)
             ->orderBy('kpis.date', 'desc')->orderBy('kpis.created_at', 'desc')->get();
